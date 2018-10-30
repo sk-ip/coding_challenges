5c351df3443a8eaec018c60b508fcdb2dec9830d
sk-ip <sudhanshukumar5459@gmail.com>
2018-10-30 13:37:45 +0530

    learned about how to create your own packages in java

---
+++ October_2018/ex2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import pack1.ClassA;
+
+class ex2
+{
+	public static void main(String args[])
+	{
+		ClassA a = new ClassA();
+		a.Display();
+	}
+
+}
---
+++ October_2018/pack1/ClassA.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class ClassA 
+{
+	public void Display()
+	{
+		System.out.println("this is class A");
+	}
+}
