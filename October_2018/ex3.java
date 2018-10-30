0b8814d8f5c7db30de965df611c777b59c403037
sk-ip <sudhanshukumar5459@gmail.com>
2018-10-30 13:47:05 +0530

    learned about how to create user defined packages in java

---
+++ October_2018/ex3.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.lang.Exception;
+
+class MyException extends Exception
+{
+	MyException(String message)
+	{
+		super(message);
+	}
+}
+
+class ex3
+{
+	public static void main(String args[])
+	{
+		int x=5,y=1000;
+		try{
+			float z = (float)x/(float)y;
+			if(z<0.01)
+				throw new MyException("number is too smaall");
+		}
+		catch(Exception e)
+		{
+			System.out.println("caught the exception");
+			System.out.println(e.getMessage());
+		}
+		finally
+		{
+			System.out.println("everything is cool");
+		}
+	}
+}
