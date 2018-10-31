ed630d030d6c8f438cc0d8411a66373c4e4b08d1
sk-ip <sudhanshukumar5459@gmail.com>
2018-10-31 09:54:29 +0530

    learned about multithreading in java by implementing the runnable interface

---
+++ October_2018/ex5.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+class A implements Runnable
+{
+	public void run()
+	{
+		for(int i=0;i<10;i++)
+			System.out.println(" "+ i);
+	}
+}
+
+class B implements Runnable
+{
+	public void run()
+	{
+		for(char ch='a'; ch<'j'; ch++)
+			System.out.println(" "+ch);
+	}
+}
+
+class ex5
+{
+	public static void main(String args[])
+	{
+		A thd1 = new A();
+		B thd2 = new B();
+		Thread th = new Thread(thd1);
+		Thread th2 = new Thread(thd2);
+		th.start();
+		th2.start();
+	}
+}
